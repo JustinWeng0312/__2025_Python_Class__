7047bc187f14a28f6b0e6754cfc7d35b21f0da42
JustinWeng0312 <jenhui0312@gmail.com>
2025-07-19 16:17:01 +0800

    list學習, 資料查詢方法

---
+++ lesson4/lesson4_2.py
@@ -0,0 +1,20 @@
+# list
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")  # 添加新水果
+fruits.append("grape")  # 再添加一個水果
+fruits.append("watermelon") # 以下是用Copilot自動補全的
+fruits.append("pineapple")
+fruits.append("mango")
+fruits.append("strawberry")
+fruits.append("blueberry")
+fruits.append("kiwi")
+fruits.append("peach")
+fruits.append("pear")
+fruits.append("plum")
+fruits.append("lemon")
+fruits.append("papaya")
+print("水果清單:", fruits)
+print(fruits[0])  # 輸出: apple
+print(fruits[1])  # 輸出: banana
+print(fruits[2])  # 輸出: cherry
+  
