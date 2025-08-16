3243c278da2f6f94b73cba70f999bb762ba7a310
JustinWeng0312 <jenhui0312@gmail.com>
2025-08-16 16:24:38 +0800

    modify

---
+++ lesson8/lesson8_7_ai.py
@@ -0,0 +1,53 @@
+import gradio as gr
+from google import genai
+from google.genai import types
+
+client = genai.Client()
+
+with gr.Blocks() as demo:
+    gr.Markdown("## Text to Summarization(總結)")
+    style_radio = gr.Radio(choices=["小學", "商業", "專業", "口語化", "條列式"], label="風格")
+    input_text = gr.Textbox(
+        label="請輸入文章", 
+        placeholder="在此輸入...", 
+        lines=10,
+        submit_btn=True
+    )
+
+    output_md = gr.Markdown(stream=True)
+
+    @gr.on(inputs=[style_radio, input_text], outputs=[output_md])
+    def summarize(style, text):
+        if style == "小學":
+            style = "請使用小學生看得懂的語法\n"
+        elif style == "商業":
+            style = "請使用商業文章的風格\n"
+        elif style == "專業":
+            style = "請使用專業的風格\n"
+        elif style == "口語化":
+            style = "請使用口語化的風格\n"
+        elif style == "條列式":
+            style = "請條列式重點\n"
+
+        response_stream = client.models.generate_content_stream(
+            model="gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                system_instruction=f"""
+                你是一個專業的總結助手，也是一個繁體中文的高手,請根據用戶提供的文章內容進行總結。
+                你需要根據用戶選擇的風格進行總結，並且確保總結的內容清晰、簡潔且易於理解。
+                目前使用者選擇的是{style}。
+                所有的回覆必須要markdown語法
+                """
+            ),
+            contents=[text]
+        )
+
+        summary = f"風格: {style}\n\n"
+        yield gr.Markdown.update(value=summary)
+        for chunk in response_stream:
+            if hasattr(chunk, "text"):
+                summary += chunk.text
+                yield gr.Markdown.update(value=summary)
+    
+
+demo.launch()
